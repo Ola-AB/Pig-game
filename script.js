d40b064ca7ac8fc6a6af2ca45eb1175d622696c9
Ola <olo2040@wp.pl>
2024-05-04 07:05:59 +0200

    added js file - selecting elements

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+// diceEl.classList.add('hidden');
