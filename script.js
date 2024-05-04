88b8704f652cdc53edf87217a07b2544c79dbd30
Ola <olo2040@wp.pl>
2024-05-04 07:20:50 +0200

    functionality of the dice roll

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+let currentScore = 0;
+
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-// diceEl.classList.add('hidden');
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./png/dice-${dice}.png`;
+  
+    // 3. Check for rolled 1: if true, switch to next player 
+    
+  });
