a8e4747210036b64e8a9e7332adb9441daee5c5e
Ola <olo2040@wp.pl>
2024-05-05 08:01:31 +0200

    added resetting the score and changing the background

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -35,5 +37,10 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch the player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
